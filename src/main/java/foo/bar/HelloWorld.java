eeed1ed66f148e536f530acb8ca1353b6c24582f
Sudhir <sudhir.mangipudi@kubota.com>
2018-05-14 13:35:09 -0500

    added java directory

--- src/main/java/foo/bar/HelloWorld.java
+++ src/main/java/foo/bar/HelloWorld.java
@@ -1,10 +1,8 @@
 package foo.bar;
 
-public class Main
-{
-	public static void main(String[] args)
-	{
-		System.out.println("Hello world");
-	}
-	
+public class HelloWorld {
+   public static void main(String[] args) {
+      // Prints "Hello, World" in the terminal window.
+      System.out.println("Hello, World");
+   }
 }
